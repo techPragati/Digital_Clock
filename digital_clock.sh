32553ff6f723b0870e0ac0e0b28c7a8873faa470
techPragati <89591718+techPragati@users.noreply.github.com>
2023-06-23 02:02:32 +0530

    Add files via upload

---
+++ digital_clock.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+<<doc
+Name : Pragati Kumari
+Date : 
+Title : 
+Sample I/P :
+Sample O/P :
+doc
+
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+blue=$'\e[1;34m'
+while true
+do
+	clear
+	echo "$blue $(date +%T)"
+	sleep 1s
+done
+
